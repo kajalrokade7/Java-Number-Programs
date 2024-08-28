fab5aca03eecd70f240e2decaeae819261d427f5
kajalrokade7 <153052327+kajalrokade7@users.noreply.github.com>
2024-08-28 21:23:41 +0530

    Add files via upload

---
+++ TwinPrime.java
@@ -0,0 +1,25 @@
+class DuckNum 
+{
+	public static void main(String[] args) 
+	{
+      int start = 1;
+	  int end = 50;
+	  while (start<=end)
+	  {
+		  int den = 2;
+		  for(; den<start; den++)
+		  {
+			  if(start%den==0)
+			  {
+				  break;
+			  }
+		  }
+		  if(den==start)
+		  { 
+	         int num = start;
+			 int 
+		  }
+		  
+	  }
+	}
+}
